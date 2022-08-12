33ad9ed836e98d497b91a0ce3032df71ef50b3ba
Tom Roberts <t.roberts@kcl.ac.uk>
2022-08-12 21:55:38 +0100

    Testing: passing auto_pete output to HTML

---
+++ auto_pete/output.py
@@ -0,0 +1,8 @@
+def output_formation():
+    output = {'D': ['helen', 'caroline'],
+              'C': ['vicky'],
+              'W': ['sian', 'christine'],
+              'F': ['mychelle'],
+              'S': ['olivia', 'keah', 'jaz']}
+
+    return output
