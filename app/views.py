11c2223fbc03d2db8191dca3ed182144e189e680
Tom Roberts <t.roberts@kcl.ac.uk>
2022-08-12 15:10:16 +0100

    WIP: convert player table to form, add template for output formation

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    player_name = request.form.get("player_name")
+    pref_defence = request.form.get("pref_defence")
+
+    return render_template("team_selection.html", player_name=player_name, pref_defence=pref_defence)
 
 
 if __name__ == "__main__":
