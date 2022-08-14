cf0282f0ab6842ec06f238fd530568d7416d1e99
Tom Roberts <t.roberts@kcl.ac.uk>
2022-08-14 22:56:01 +0100

    Update forms.py with FieldList and routes.py

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, IntegerField, FieldList, FormField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-
-class TeamSelectionForm(FlaskForm):
+class PlayerPrefForm(FlaskForm):
     player_name  = StringField('Player Name', validators=[DataRequired(), Length(min=2, max=20)])
     pref_defence = IntegerField('Defender', validators=[DataRequired()])
     pref_central = IntegerField('Central', validators=[DataRequired()])
     pref_winger  = IntegerField('Winger', validators=[DataRequired()])
     pref_forward = IntegerField('Forward', validators=[DataRequired()])
-    submit       = SubmitField('Generate Formation')
+
+
+class TeamSelectionForm(FlaskForm):
+    PlayerList = FieldList(FormField(PlayerPrefForm), min_entries=2)
+    submit = SubmitField('Generate Formation')
--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,52 @@
-from flask import render_template, request
+from flask import render_template, request, flash
 from app import app
-from app.forms import RegistrationForm, TeamSelectionForm
+from app.forms import TeamSelectionForm
 from auto_pete.output import output_formation
+import logging
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = TeamSelectionForm()
-    if form.validate_on_submit():
+
+    if form.is_submitted():
         flash(f'Team selection submitted!', 'success')
-        return redirect(url_for('home'))
+        app.logger.info("TeamSelectionForm submitted.")
+
+        player_formation_prefs = form.PlayerList.data
+
+        # TODO: Run AUTO_PETE, e.g.:
+        #
+        # formation = run_auto_pete(player_formation_prefs)
+        # INPUT: form.PlayerList.data – need to convert/format this so compatible with auto_pete code
+        # OUTPUT: formation – formatted so can render on team_selection.html
+
+        # For now:
+        # create dummy values so I can create HTML page formatting
+
+        # for field in form.PlayerList:
+        #     print(field.player_name.data)
+        #     print(field.pref_defence.data)
+        #
+        # for value in form.PlayerList.data:
+        #     print(value)
+
+        formation = output_formation()
+        defenders = formation['D']
+        central   = formation['C']
+        wingers   = formation['W']
+        forwards  = formation['F']
+        subs      = formation['S']
+
+        return render_template("team_selection.html",
+                               formation=formation,
+                               defenders=defenders,
+                               central=central,
+                               wingers=wingers,
+                               forwards=forwards,
+                               subs=subs)
+
     return render_template("index.html", form=form)
-
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/submit", methods=["POST"])
-def generate_team_selection():
-    player_name = request.form.get("player_name")
-    pref_defence = request.form.get("pref_defence")
-
-    formation = output_formation()
-
-    return render_template("team_selection.html",
-                           player_name=player_name, pref_defence=pref_defence, formation=formation)
 
 
 if __name__ == "__main__":
