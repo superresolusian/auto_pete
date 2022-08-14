1f252fb6dfe370392098957a602d6e1a23fe9ca5
Tom Roberts <t.roberts@kcl.ac.uk>
2022-08-14 22:56:24 +0100

    Capitalise output formation names

--- auto_pete/output.py
+++ auto_pete/output.py
@@ -1,8 +1,8 @@
 def output_formation():
-    output = {'D': ['helen', 'caroline'],
-              'C': ['vicky'],
-              'W': ['sian', 'christine'],
-              'F': ['mychelle'],
-              'S': ['olivia', 'keah', 'jaz']}
+    output = {'D': ['Helen', 'Caroline'],
+              'C': ['Vicky'],
+              'W': ['Sian', 'Christine'],
+              'F': ['Mychelle'],
+              'S': ['Olivia', 'Keah', 'Jaz']}
 
     return output
