a34448c03cbacde5aa800d4a1c7ee9aab4da9965
Tom Roberts <t.roberts@kcl.ac.uk>
2022-11-12 21:40:41 +0000

    Update output.py with TODO

--- auto_pete/output.py
+++ auto_pete/output.py
@@ -1,4 +1,10 @@
 def output_formation():
+
+    # TODO: input needs to be Team.players list
+    #  - e.g. Roses.players = [Sian, Vicky, Jaz, Mychelle, Shafa, Helen, Caroline, Keah]
+    #  containing Player objects:
+    #  - e.g. Sian = Player('Sian', 1, 0, 1, 0)
+
     output = {'D': ['Helen', 'Caroline'],
               'C': ['Vicky'],
               'W': ['Sian', 'Christine'],
