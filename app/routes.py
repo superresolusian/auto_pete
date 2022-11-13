8a326b1fabd0cb6efbc86751837753dd162a3b3b
Tom Roberts <t.roberts@kcl.ac.uk>
2022-11-13 00:03:06 +0000

    Update output.py and routes.py - e2e now working
    TODO: test web app player positions accurate and match original notebook

--- app/routes.py
+++ app/routes.py
@@ -13,68 +13,51 @@ def home():
         flash(f'Team selection submitted!', 'success')
         app.logger.info("TeamSelectionForm submitted.")
 
-    #     player_formation_prefs = form.PlayerList.data
-    #
-    #     # TODO: remove these comments once shifted AUTO_PETE code to optimise_team():
-    #     #
-    #     # OLD COMMENTS ON HOW TO RUN AUTO_PETE (superseded by optimise_team() )
-    #
-    #     # formation = run_auto_pete(player_formation_prefs)
-    #     # INPUT: form.PlayerList.data – need to convert/format this so compatible with auto_pete code
-    #     # OUTPUT: formation – formatted so can render on team_selection.html
-    #
-    #     # For now:
-    #     # create dummy values so I can create HTML page formatting
-    #
-    #     # for field in form.PlayerList:
-    #     #     print(field.player_name.data)
-    #     #     print(field.pref_defence.data)
-    #     #
-    #     # for value in form.PlayerList.data:
-    #     #     print(value)
-    #
-    #     formation = output_formation()
-    #     defenders = formation['D']
-    #     central   = formation['C']
-    #     wingers   = formation['W']
-    #     forwards  = formation['F']
-    #     subs      = formation['S']
-    #
-    #     return render_template("team_selection.html",
-    #                            formation=formation,
-    #                            defenders=defenders,
-    #                            central=central,
-    #                            wingers=wingers,
-    #                            forwards=forwards,
-    #                            subs=subs)
-
     return render_template("index.html", form=form)
 
 
 @app.route("/team_selection", methods=['GET', 'POST'])
 def optimise_team():
 
+    # Retrieve team preference form inputs
     team_form = request.form
 
-    # Create dictionaries to pass to HTML using dictionary comprehension
-    # Syntax: {player: team_form[player] ... }
-    # Gives key: value pairs
-    player_name_submitted = {player: team_form[player] for player in team_form if "player_name" in player}
-    pref_defence_submitted = {player: team_form[player] for player in team_form if "pref_defence" in player}
-    pref_central_submitted = {player: team_form[player] for player in team_form if "pref_central" in player}
-    pref_winger_submitted = {player: team_form[player] for player in team_form if "pref_winger" in player}
-    pref_forward_submitted = {player: team_form[player] for player in team_form if "pref_forward" in player}
+    # Convert form to lists
+    player_name_submitted = [team_form[player] for player in team_form if "player_name" in player]
+    pref_defence_submitted = [team_form[player] for player in team_form if "pref_defence" in player]
+    pref_central_submitted = [team_form[player] for player in team_form if "pref_central" in player]
+    pref_winger_submitted = [team_form[player] for player in team_form if "pref_winger" in player]
+    pref_forward_submitted = [team_form[player] for player in team_form if "pref_forward" in player]
 
-    # TODO: change above to send results from AUTO_PETE to team_selection.html
-    #  - current test code simply sends form field inputs
-    #  - I think: send team_form to AUTO_PETE, return results from output_formation() and pass to team_selection.html
+    # Pass list of lists to AUTO_PETE
+    num_players = len(player_name_submitted)
+    team_preferences = []
+    for n in range(num_players):
+        team_preferences.append([
+            player_name_submitted[n],
+            pref_defence_submitted[n],
+            pref_central_submitted[n],
+            pref_winger_submitted[n],
+            pref_forward_submitted[n]
+        ])
+
+    # Run AUTO_PETE
+    formation = output_formation(team_preferences)
+
+    # Parse AUTO_PETE formation for display
+    defenders = formation['D']
+    central   = formation['C']
+    wingers   = formation['W']
+    forwards  = formation['F']
+    subs      = formation['S']
 
     return render_template("team_selection.html",
-                           player_name_submitted=player_name_submitted,
-                           pref_defence_submitted=pref_defence_submitted,
-                           pref_central_submitted=pref_central_submitted,
-                           pref_winger_submitted=pref_winger_submitted,
-                           pref_forward_submitted=pref_forward_submitted
+                           formation=formation,
+                           defenders=defenders,
+                           central=central,
+                           wingers=wingers,
+                           forwards=forwards,
+                           subs=subs
                            )
 
 
--- auto_pete/output.py
+++ auto_pete/output.py
@@ -1,14 +1,42 @@
-def output_formation():
+from auto_pete.players import Player
+from auto_pete.team import Team
 
-    # TODO: input needs to be Team.players list
-    #  - e.g. Roses.players = [Sian, Vicky, Jaz, Mychelle, Shafa, Helen, Caroline, Keah]
-    #  containing Player objects:
-    #  - e.g. Sian = Player('Sian', 1, 0, 1, 0)
 
-    output = {'D': ['Helen', 'Caroline'],
-              'C': ['Vicky'],
-              'W': ['Sian', 'Christine'],
-              'F': ['Mychelle'],
-              'S': ['Olivia', 'Keah', 'Jaz']}
+def output_formation(team_preferences):
 
-    return output
+    print('Team Preferences:', team_preferences)
+
+    # Instantiate Team object
+    # TODO: add Team Name box to front end
+    Roses = Team('Roses')
+
+    # Add Player objects to Team object
+    # TODO: below seems clunky
+    #  - seek cleaner way of adding Player objects to Team class
+    player_list = []
+    for p in range(len(team_preferences)):
+        player_list.append(Player(
+            team_preferences[p][0],
+            int(team_preferences[p][1]),
+            int(team_preferences[p][2]),
+            int(team_preferences[p][3]),
+            int(team_preferences[p][4])
+        ))
+
+    Roses.players = player_list
+    Roses.num_subs = len(Roses.players) - Roses.teamsize
+
+    # Calculate Player Costs
+    player_costs = []
+    for player in Roses.players:
+        player_costs.append(player.player_costs())
+
+    team_cost_matrix = Roses.team_cost_matrix()
+
+    formation_with_subs = Roses.formation.copy()
+    for i in range(Roses.num_subs):
+        formation_with_subs.append('S')
+
+    formation_output = Roses.cost_matrix_to_formation(team_cost_matrix, Roses.get_player_names(), formation_with_subs)
+
+    return formation_output
