23d5e72e321548906ac2d6ea006b2c5fb8f43054
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-24 22:05:50 +0000

    Small fix after various refactoring

--- run_auto_pete.py
+++ run_auto_pete.py
@@ -26,6 +26,6 @@ print(f'Num Players: {Roses.num_players}')
 print(f'Num Subs: {Roses.num_subs}')
 print(f'Num Match Periods: {Roses.num_periods}')
 
-output_formation = Roses.cost_matrix_to_formation(team_cost_matrix, Roses.get_player_names(), formation_with_subs)
+output_formation = Roses.cost_matrix_to_formation(team_cost_matrix, Roses.get_player_names(), Roses.formation)
 print("Formation", output_formation)
 
