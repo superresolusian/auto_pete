560e13062ef45de3fe81e7d63a4a4b5ed6d634dc
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-25 23:26:45 +0000

    Adds match format and substitutes inputs
    Pass HTML button inputs to session object – nb: feels a bit clunky, potentially better to do with the form itself somehow

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, flash
+from flask import render_template, request, redirect, flash, session, url_for
 
 from app import app
 from app.forms import TeamSelectionForm
@@ -7,17 +7,51 @@ from auto_pete.output import output_formation
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    form = TeamSelectionForm()
 
-    if form.is_submitted():
-        flash(f'Team selection submitted!', 'success')
-        app.logger.info("TeamSelectionForm submitted.")
+    if request.method == 'GET':
+        form = TeamSelectionForm()
 
-    return render_template("index.html", form=form)
+        if form.is_submitted():
+            flash(f'Team selection submitted!', 'success')
+            app.logger.info("TeamSelectionForm submitted.")
+
+        return render_template("index.html", form=form)
+
+
+@app.route("/set_match_format", methods=['GET', 'POST'])
+def set_match_format():
+
+    # Retrieve number of players based on match format (5-a-side, 11-a-side, etc.)
+    num_players = request.form['num_players']
+    session['num_players'] = int(num_players)
+
+    return redirect(url_for('home'))
+    # return redirect(request.referrer)  # effectively: return redirect(url_for('home'))
+
+
+@app.route("/set_num_subs", methods=['GET', 'POST'])
+def set_num_subs():
+
+    # Retrieve number of subs input
+    num_subs = request.form['num_subs']
+    session['num_subs'] = int(num_subs)
+
+    return redirect(url_for('home'))
+    # return redirect(request.referrer)  # effectively: return redirect(url_for('home'))
 
 
 @app.route("/team_selection", methods=['GET', 'POST'])
 def optimise_team():
+
+    # initialise if match format or number of subs not specified
+    if not session['num_players']:
+        session['num_players'] = 7
+    if not session['num_subs']:
+        session['num_subs'] = 3
+
+    app.logger.info('route: optimise_team')
+    app.logger.info(f'num_players: {session["num_players"]}')
+    app.logger.info(f'num_subs: {session["num_subs"]}')
 
     # Retrieve team preference form inputs
     team_form = request.form
