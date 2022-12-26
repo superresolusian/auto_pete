ad69e5f7372c9d3a2622cb6b784e3a7ec5be8c1f
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-26 21:23:10 +0000

    Dynamically control TeamSelectionForm size
    First working implementation of buttons to control frontend team selection form size
    - Important workaround added so that min_entries can be dynamically changed within FieldList

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
 from flask import render_template, request, redirect, flash, session, url_for
+from wtforms import FieldList, FormField
 
 from app import app
-from app.forms import TeamSelectionForm
+from app.forms import TeamSelectionForm, PlayerPrefForm
 from auto_pete.output import output_formation
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+
+    # initialise session parameters to control number of rows in displayed TeamSelectionForm
+    session['num_players'] = 5
+    session['num_subs'] = 0
 
     if request.method == 'GET':
         form = TeamSelectionForm()
@@ -22,22 +27,30 @@ def home():
 def set_match_format():
 
     # Retrieve number of players based on match format (5-a-side, 11-a-side, etc.)
-    num_players = request.form['num_players']
-    session['num_players'] = int(num_players)
+    num_players = int(request.form['num_players'])
+    session['num_players'] = num_players
 
-    return redirect(url_for('home'))
-    # return redirect(request.referrer)  # effectively: return redirect(url_for('home'))
+    num_subs = session['num_subs']
+
+    # TODO: remove -1 when goalkeeper added as position
+    form = dynamically_update_form(num_players - 1 + num_subs)
+
+    return render_template("index.html", form=form)
 
 
 @app.route("/set_num_subs", methods=['GET', 'POST'])
 def set_num_subs():
 
     # Retrieve number of subs input
-    num_subs = request.form['num_subs']
+    num_subs = int(request.form['num_subs'])
     session['num_subs'] = int(num_subs)
 
-    return redirect(url_for('home'))
-    # return redirect(request.referrer)  # effectively: return redirect(url_for('home'))
+    num_players = session['num_players']
+
+    # TODO: remove -1 when goalkeeper added as position
+    form = dynamically_update_form(num_players - 1 + num_subs)
+
+    return render_template("index.html", form=form)
 
 
 @app.route("/team_selection", methods=['GET', 'POST'])
@@ -93,6 +106,31 @@ def optimise_team():
                            forwards=forwards,
                            subs=subs
                            )
+
+
+def dynamically_update_form(min_entries):
+    """
+    Dynamically update TeamSelectionForm
+
+    **IMPORTANT**
+    It is not possible to dynamically update min_entries in a FieldList.
+    Workaround: https://stackoverflow.com/a/54958358/20867242
+
+    :min_entries: number of player entries in TeamSelectionForm
+    :return: new form object
+    """
+
+    # Subclass form
+    class LocalForm(TeamSelectionForm):
+        pass
+
+    # Bind new field
+    LocalForm.PlayerList = FieldList(FormField(PlayerPrefForm), min_entries=min_entries)
+
+    # Use new form
+    form = LocalForm()
+
+    return form
 
 
 if __name__ == "__main__":
